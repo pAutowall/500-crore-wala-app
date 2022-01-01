70ca08d689b1fd253c55efc13460c39cb31e17a4
pAutowall <yogeshvernekar99@gmail.com>
2022-01-01 21:34:32 +0530

    add dynamic card js (WIP) and deleted static cards

--- dashboard.php
+++ dashboard.php
@@ -3,136 +3,92 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Dashboard</title>
     <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="css/my-login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-  </head>
-  <body>
+    <script src="js/cards.js" defer></script>
+</head>
+
+<body>
     <!--header area start-->
     <header>
-      <div class="left_area">
-        <h3>Food4<span>Thought</span></h3>
-      </div>
-      <div class="right_area">
-        <a href="logout.php" class="logout_btn">Logout</a>
-      </div>
+        <div class="left_area">
+            <h3>Food4<span>Thought</span></h3>
+        </div>
+        <div class="right_area">
+            <a href="logout.php" class="logout_btn">Logout</a>
+        </div>
     </header>
     <!--header area end-->
     <!--sidebar start-->
     <section>
-    <div class="sidebar">
-      <center>
-        <img src="img/1.png" class="profile_image" alt="">
-        <h4>Demo User</h4>
-      </center>
-      <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-      <a href="#popup1"><i class="fas fa-cogs"></i><span>Create Request</span></a>
-      <a href="#"><i class="fas fa-table"></i><span>ABC</span></a>
-      <a href="#"><i class="fas fa-th"></i><span>ABC</span></a>
-      <a href="#"><i class="fas fa-info-circle"></i><span>ABC</span></a>
-      <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
-    </div>
-    <!--sidebar end-->
-  </section>
+        <div class="sidebar">
+            <center>
+                <img src="img/1.png" class="profile_image" alt="">
+                <h4>Demo User</h4>
+            </center>
+            <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+            <a href="#popup1"><i class="fas fa-cogs"></i><span>Create Request</span></a>
+            <a href="javascript:gclick()"><i class="fas fa-table"></i><span>Create Card</span></a>
+            <a href="#"><i class="fas fa-th"></i><span>ABC</span></a>
+            <a href="#"><i class="fas fa-info-circle"></i><span>ABC</span></a>
+            <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+        </div>
+        <!--sidebar end-->
+    </section>
 
     <!-- <div class="content"></div> -->
 
 
 
-    <div class="container-1">
-        <div class="courses-container">
-          <div class="course">
-            <div class="course-preview">
-              <img src="img/map.jpg" height="200" width="200">
+    <div id="box1" class="container-1">
+        
+        </div>
+
+        <div id="popup1" class="overlay">
+            <div class="popup">
+                <h2>Donation Form</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Food type</label>
+                            <input id="name" type="text">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Quantity</label>
+                            <input id="email" type="email">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Location</label>
+                            <input id="password" type="password" name="password">
+                        </div>
+
+
+
+                        <div class="form-group m-0">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="course-info">
-              <div class="progress-container">
-                <div class="progress"></div>
-              </div>
-              <h6>Katraj, Pune</h6>
-              <h2>Food Packets for 50 People</h2>
-              <button class="btn">More Info</button>
-              
-            </div>
-          </div>
-          <div class="courses-container">
-          <div class="course">
-            <div class="course-preview">
-              <img src="img/map.jpg" height="200" width="200">
-            </div>
-            <div class="course-info">
-              <div class="progress-container">
-                <div class="progress"></div>
-              </div>
-              <h6>Dhankawadi, Pune</h6>
-              <h2>250 Meals Needed @ Dhankawadi </h2>
-              <button class="btn">More Info</button>
-              
-            </div>
-          </div>
-          <div class="courses-container">
-          <div class="course">
-            <div class="course-preview">
-              <img src="img/map.jpg" height="200" width="200">
-            </div>
-            <div class="course-info">
-              <div class="progress-container">
-                <div class="progress"></div>
-              </div>
-              <h6>Kothrud, Pune</h6>
-              <h2>Meal for 200</h2>
-              <button class="btn">More Info</button>
-              
-            </div>
-          </div>
+        </div>
+        <div class="container-2">
+
         </div>
 
 
-      </div>
-    </div>
+</body>
 
-    <div id="popup1" class="overlay">
-      <div class="popup">
-        <h2>Donation Form</h2>
-        <a class="close" href="#">&times;</a>
-        <div class="content">
-          <form >
-            <div class="form-group">
-              <label for="name">Food type</label>
-              <input id="name" type="text">
-
-            </div>
-
-            <div class="form-group">
-              <label for="email">Quantity</label>
-              <input id="email" type="email">
-
-            </div>
-
-            <div class="form-group">
-              <label for="password">Location</label>
-              <input id="password" type="password" name="password">
-            </div>
-
-
-
-            <div class="form-group m-0">
-              <button type="submit" class="btn btn-primary btn-block">
-                Submit
-              </button>
-            </div>
-            </form>
-        </div>
-      </div>
-    </div>
-    <div class="container-2">
-
-      </div>
-
-
-  </body>
 </html>
